c4a2a8ca01bd6a050facdf503947858a27549e7a
majorz <zarchaoz@gmail.com>
2017-11-03 09:35:51 +0200

    Regex tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+extern crate regex;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+mod tokenize;
+
+use tokenize::tokenize;
+
+
 fn main() {
-    println!("Nel - experimental programming language");
+   let mut f = File::open("nel/tokenize.nel").expect("file not found");
+
+    let mut input = String::new();
+    f.read_to_string(&mut input)
+        .expect("something went wrong reading the file");
+
+   let _tokens = tokenize(&input);
 }
---
+++ src/tokenize.rs
@@ -0,0 +1,117 @@
+use regex::Regex;
+use std::cmp;
+
+#[derive(Debug, Copy, Clone)]
+pub enum Token {
+   Space,
+   Tab,
+   NewLine,
+   Power,
+   Equal,
+   Unequal,
+   LessEqual,
+   GreaterEqual,
+   Increase,
+   Decrease,
+   Dot,
+   Caret,
+   Assign,
+   Add,
+   Subtract,
+   Multiply,
+   Divide,
+   Bar,
+   Colon,
+   ParenLeft,
+   ParenRight,
+   BracketLeft,
+   BracketRight,
+   AngleLeft,
+   AngleRight,
+   Comment,
+   Accent,
+   String,
+   Ident,
+   Number,
+}
+
+const REGEX_MAP: [(&'static str, Token); 30] = [
+   (r"^ +",    Token::Space),
+   (r"^\t+",   Token::Tab),
+   (r"^\n",    Token::NewLine),
+   (r"^\*\*",  Token::Power),
+   (r"^==",    Token::Equal),
+   (r"^!=",    Token::Unequal),
+   (r"^<=",    Token::LessEqual),
+   (r"^>=",    Token::GreaterEqual),
+   (r"^\+=",   Token::Increase),
+   (r"^-=",    Token::Decrease),
+   (r"^\.",    Token::Dot),
+   (r"^\^",    Token::Caret),
+   (r"^=",     Token::Assign),
+   (r"^\+",    Token::Add),
+   (r"^-",     Token::Subtract),
+   (r"^\*",    Token::Multiply),
+   (r"^/",     Token::Divide),
+   (r"^\|",    Token::Bar),
+   (r"^:",     Token::Colon),
+   (r"^\(",    Token::ParenLeft),
+   (r"^\)",    Token::ParenRight),
+   (r"^\[",    Token::BracketLeft),
+   (r"^\]",    Token::BracketRight),
+   (r"^<",     Token::AngleLeft),
+   (r"^>",     Token::AngleRight),
+   (r"^#[^\n]",                  Token::Comment),
+   (r"^`(?:\\\)|[^)\s])+",       Token::Accent),
+   (r"^'(?:\\'|[^'])*'",         Token::String),
+   (r"^[_A-Za-z]+[_A-Za-z0-9]*", Token::Ident),
+   (r"^[0-9]+\.*[0-9]*",         Token::Number),
+];
+
+pub fn tokenize(mut input: &str) -> Vec<Token> {
+   println!("{:?}", &input[..cmp::min(30, input.len())]);
+
+   let mut count = 0;
+   let mut tokens = vec![];
+
+   let regex_map = get_regex_map();
+
+   while !input.is_empty() {
+      match match_token(input, &regex_map) {
+         Some((token, span)) => {
+            println!("{:?} {}", token, span);
+            tokens.push(token);
+            input = &input[span..];
+            println!("{:?}", &input[..cmp::min(30, input.len())]);
+         },
+         None => panic!("Unrecognized: {:?}", input),
+      }
+
+      count += 1;
+      if count == 10000 {
+         break;
+      }
+   }
+
+   tokens
+}
+
+fn get_regex_map() -> Vec<(Regex, Token)> {
+   let mut regex_map = vec![];
+
+   for &(expression, token) in &REGEX_MAP {
+      regex_map.push((Regex::new(expression).unwrap(), token));
+   }
+
+   regex_map
+}
+
+fn match_token(input: &str, regex_map: &[(Regex, Token)]) -> Option<(Token, usize)> {
+   for &(ref re, token) in regex_map {
+      if let Some(res) = re.find(input) {
+         return Some((token, res.end()));
+      }
+   }
+
+   None
+}
