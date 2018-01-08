df14bf04777db6f91567e934a25ac9ed003e838e
majorz <zarchaoz@gmail.com>
2018-01-09 00:13:18 +0200

    Expressions and statements

--- src/parse.rs
+++ src/parse.rs
@@ -1,6 +1,6 @@
 use tokenize::{TokenType, Token};
 
-//const INDENT: usize = 3;
+const INDENT: usize = 3;
 
 pub struct Parser<'a, 'b> {
    tokens: &'a [Token],
@@ -29,7 +29,7 @@ impl<'a, 'b> Parser<'a, 'b> {
       let _ = current;
    }
 
-   fn consume_fn(&self, pos: usize, _indent: usize) -> Option<usize> {
+   fn consume_fn(&self, pos: usize, indent: usize) -> Option<usize> {
       let mut current = pos;
 
       if let Some(pos) = self.consume_fn_def(current) {
@@ -38,7 +38,7 @@ impl<'a, 'b> Parser<'a, 'b> {
          return None;
       }
 
-      if let Some(pos) = self.consume_fn_body(current) {
+      if let Some(pos) = self.consume_block(current, indent + 1) {
          current = pos;
       } else {
          return None;
@@ -58,9 +58,9 @@ impl<'a, 'b> Parser<'a, 'b> {
 
       current = self.skip_space(current);
 
-      let name = if let Some((pos, name)) = self.consume_ident(current) {
+      let name = if let Some((pos, ident)) = self.consume_ident(current) {
          current = pos;
-         name
+         ident
       } else {
          return None;
       };
@@ -111,8 +111,138 @@ impl<'a, 'b> Parser<'a, 'b> {
       }
    }
 
-   fn consume_fn_body(&self, pos: usize) -> Option<usize> {
-      Some(pos)
+   fn consume_block(&self, pos: usize, indent: usize) -> Option<usize> {
+      let mut current = pos;
+      let mut consumed = false;
+
+      loop {
+         if let Some(pos) = self.consume_statement(current, indent) {
+            current = pos;
+            consumed = true;
+         } else {
+            if consumed {
+               return Some(current);
+            } else {
+               return None;
+            }
+         }
+      }
+   }
+
+   fn consume_statement(&self, pos: usize, indent: usize) -> Option<usize> {
+      let mut current = pos;
+
+      if let Some(pos) = self.consume_indent_space(current, indent) {
+         current = pos;
+      } else {
+         return None;
+      }
+
+      if let Some(pos) = self.consume_assignment(current) {
+         current = pos;
+      } else {
+         return None;
+      }
+
+      if let Some(pos) = self.consume_empty_lines(current) {
+         current = pos;
+      } else {
+         return None;
+      }
+
+      Some(current)
+   }
+
+   fn consume_assignment(&self, pos: usize) -> Option<usize> {
+      let mut current = pos;
+
+      let variable = if let Some((pos, ident)) = self.consume_ident(current) {
+         current = pos;
+         ident
+      } else {
+         return None;
+      };
+
+      current = self.skip_space(current);
+
+      if let Some(pos) = self.consume_token_type(current, TokenType::Assign) {
+         current = pos;
+      } else {
+         return None;
+      }
+
+      current = self.skip_space(current);
+
+      if let Some(pos) = self.consume_expression(current) {
+         current = pos;
+      } else {
+         return None;
+      }
+
+      println!("[{}-{}] {} = ", pos, current - 1, variable);
+
+      Some(current)
+   }
+
+   fn consume_expression(&self, pos: usize) -> Option<usize> {
+      if let Some((pos, _number)) = self.consume_number(pos) {
+         Some(pos)
+      } else if let Some((pos, _ident)) = self.consume_ident(pos) {
+         Some(pos)
+      } else if let Some(pos) = self.consume_list(pos) {
+         Some(pos)
+      } else {
+         None
+      }
+   }
+
+   fn consume_list(&self, pos: usize) -> Option<usize> {
+      let mut current = pos;
+
+      if let Some(pos) = self.consume_token_type(current, TokenType::BracketLeft) {
+         current = pos;
+      } else {
+         return None;
+      }
+
+      current = self.consume_list_expressions(current);
+
+      if let Some(pos) = self.consume_token_type(current, TokenType::BracketRight) {
+         current = pos;
+      } else {
+         return None;
+      }
+
+      println!("[{}-{}] [..]", pos, current - 1);
+
+      Some(current)
+   }
+
+   fn consume_list_expressions(&self, pos: usize) -> usize {
+      let mut current = pos;
+
+      current = self.skip_white_space(current);
+
+      loop {
+         if let Some(pos) = self.consume_expression(current) {
+            current = pos;
+
+            current = self.skip_white_space(current)
+         } else {
+            return current;
+         }
+      }
+   }
+
+   fn consume_indent_space(&self, pos: usize, indent: usize) -> Option<usize> {
+      if let Some(token) = self.tokens[pos..].first() {
+         if token.ty == TokenType::Space && token.span == indent * INDENT {
+            println!("[{}] indent {}", pos, indent);
+            return Some(pos + 1);
+         }
+      }
+
+      None
    }
 
    fn consume_exact_ident(&self, pos: usize, ident: &str) -> Option<usize> {
@@ -129,15 +259,11 @@ impl<'a, 'b> Parser<'a, 'b> {
    }
 
    fn consume_ident(&self, pos: usize) -> Option<(usize, &'b str)> {
-      if let Some(token) = self.tokens[pos..].first() {
-         if token.ty == TokenType::Ident {
-            let ident = &self.input[token.pos..token.pos + token.span];
-            println!("[{}] \"{}\"", pos, ident);
-            return Some((pos + 1, ident));
-         }
-      }
+      self.consume_token_string(pos, TokenType::Ident)
+   }
 
-      None
+   fn consume_number(&self, pos: usize) -> Option<(usize, &'b str)> {
+      self.consume_token_string(pos, TokenType::Number)
    }
 
    fn consume_empty_lines(&self, pos: usize) -> Option<usize> {
@@ -185,6 +311,18 @@ impl<'a, 'b> Parser<'a, 'b> {
          println!("[{}] eof", pos);
          Some(pos)
       }
+   }
+
+   fn consume_token_string(&self, pos: usize, ty: TokenType) -> Option<(usize, &'b str)> {
+      if let Some(token) = self.tokens[pos..].first() {
+         if token.ty == ty {
+            let ident = &self.input[token.pos..token.pos + token.span];
+            println!("[{}] {:?} \"{}\"", pos, ty, ident);
+            return Some((pos + 1, ident));
+         }
+      }
+
+      None
    }
 
    fn consume_token_type(&self, pos: usize, ty: TokenType) -> Option<usize> {
