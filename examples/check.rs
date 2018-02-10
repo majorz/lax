0a9c10f1c8c1c7ae8869269770e540cb64ddd204
majorz <zarchaoz@gmail.com>
2018-02-10 18:04:29 +0200

    Descend and recurse properly

--- examples/check.rs
+++ examples/check.rs
@@ -1,20 +1,11 @@
 extern crate nel;
 
-use std::fmt::Debug;
-
 use nel::tokenize::*;
 
 fn main() {
-   let string = "(+100.0)";
-   let tokens = tokenize(string);
+   let string = "(100.0+4)*0.2-0.6";
 
-/*
-   tokens.iter().enumerate().for_each(
-      |(i, token)| println!(
-         "[{}] {:?} - {}", i, token.syn, token.span
-      )
-   );
-*/
+   let tokens = tokenize(string);
 
    let syns = tokens
       .iter()
@@ -23,77 +14,68 @@ fn main() {
 
    let mut peeker = Peeker::new(&syns);
 
-   expression(&mut peeker);
+   let result = expression(&mut peeker);
+
+   if let Some(_) = result {
+      println!("done");
+   } else {
+      println!("nope");
+   }
 
    println!("remaining: {}", peeker.remaining());
+
    let count = syns.len() - peeker.remaining();
+
    syns[..count].iter().enumerate().for_each(
       |(i, item)|
-         println!("[{}] {:?}", i, item)
+         println!("[{:03}] {:?}", i, item)
    );
 
-   println!("=======");
+   println!("======");
 
    syns[count..].iter().enumerate().for_each(
       |(i, item)|
-         println!("[{}] {:?}", count + i, item)
+         println!("[{:03}] {:?}", count + i, item)
    );
+
 }
 
-type Match = Option<(Syn, usize, usize)>;
+fn expression(peeker: &mut Peeker) -> Option<usize> {
+   let mut pos = peeker.descend(single)?;
 
-fn expression(peeker: &mut Peeker<Syn>) -> Option<()> {
-   single(peeker)?;
+   loop {
+      let operators = [
+         &Syn::Add,
+         &Syn::Subtract,
+         &Syn::Multiply,
+         &Syn::Divide,
+      ];
 
-   let operators = [
-      &Syn::Add,
-      &Syn::Subtract,
-      &Syn::Multiply,
-      &Syn::Divide,
-   ];
-
-/*
-   ((+100.0 - -100) + 100 + 400 + (30 - 10)) 100 600
-   123    3   3  32   2 2   2 2   233   3321 1 1 1 1
-
-*/
-   peeker.optional(&Syn::Space);
-
-   if peeker.optional_from_slice(&operators) {
-
-      peeker.optional(&Syn::Space);
-
-      single(peeker)?;
+      if peeker.optional_from_slice(&operators).is_some() {
+         if let Some(i) = peeker.descend(single) {
+            pos = i;
+         } else {
+            break;
+         }
+      } else {
+         break;
+      }
    }
 
-   peeker.commit()
+   peeker.adjust(pos)
 }
 
-fn parens(peeker: &mut Peeker<Syn>) -> Option<()> {
-   peeker.debug("pre-paren-left");
-
+fn parens(peeker: &mut Peeker) -> Option<usize> {
    peeker.next(&Syn::ParenLeft)?;
 
-   peeker.optional(&Syn::Space);
-
-   peeker.debug("pre-expression");
-
-   expression(peeker)?;
-
-   peeker.debug("post-expression");
-
-   peeker.optional(&Syn::Space);
+   peeker.descend(expression)?;
 
    peeker.next(&Syn::ParenRight)?;
 
-   peeker.debug("post-paren-right");
-
    peeker.commit()
 }
 
-fn single(peeker: &mut Peeker<Syn>) -> Option<()> {
-   peeker.push();
-
+fn single(peeker: &mut Peeker) -> Option<usize> {
    let fns = [
       ident,
       number,
@@ -101,124 +83,121 @@ fn single(peeker: &mut Peeker<Syn>) -> Option<()> {
    ];
 
    for f in &fns {
-      if f(peeker).is_some() {
-         return peeker.pop();
+      if let Some(pos) = peeker.descend(*f) {
+         return peeker.adjust(pos);
       }
    }
 
-   peeker.pop()
+   peeker.reset()
 }
 
-fn ident(peeker: &mut Peeker<Syn>) -> Option<()> {
+fn ident(peeker: &mut Peeker) -> Option<usize> {
    peeker.next(&Syn::Ident)?;
    peeker.commit()
 }
 
-fn number(peeker: &mut Peeker<Syn>) -> Option<()> {
-   if !peeker.optional(&Syn::Add) {
-      peeker.optional(&Syn::Subtract);
-   }
-
-   peeker.next(&Syn::Digits)?;
-
-   if peeker.optional(&Syn::Dot) {
-      peeker.optional(&Syn::Digits);
+fn number(peeker: &mut Peeker) -> Option<usize> {
+   if peeker.optional(&Syn::Digits).is_some() {
+      if peeker.optional(&Syn::Dot).is_some() {
+         peeker.optional(&Syn::Digits);
+      }
+   } else {
+      peeker.next(&Syn::Dot)?;
+      peeker.next(&Syn::Digits)?;
    }
 
    peeker.commit()
 }
 
-pub struct Peeker<'s, T: 's> {
-   stack: Vec<&'s [T]>,
-   peek: &'s [T],
+#[derive(Clone)]
+pub struct Peeker<'s> {
+   input: &'s [Syn],
+   start: usize,
+   peek: usize,
 }
 
-impl<'s, T> Peeker<'s, T> where T: PartialEq + Debug {
-   fn new(input: &'s [T]) -> Self {
+impl<'s> Peeker<'s> {
+   fn new(input: &'s [Syn]) -> Self {
       Peeker {
-         stack: vec![input],
-         peek: input,
+         input: input,
+         start: 0,
+         peek: 0,
       }
    }
 
-   fn push(&mut self) {
-      self.stack.push(self.peek);
+   fn step(&mut self) -> Option<usize> {
+      self.peek += 1;
+      Some(self.peek)
    }
 
-   fn pop(&mut self) -> Option<()> {
-      self.stack.pop();
-      Some(())
+   fn current(&self) -> Option<&'s Syn> {
+      self.input.get(self.peek)
    }
 
-   fn commit(&mut self) -> Option<()> {
-//      debug_assert!(self.peek.len() != self.stack.len());
-      //let span = self.stack.len() - self.peek.len();
-      debug_assert!(!self.stack.is_empty());
-      *self.stack.last_mut().unwrap() = self.peek;
-      Some(())
+   fn commit(&mut self) -> Option<usize> {
+      debug_assert!(self.peek != self.start);
+      self.start = self.peek;
+      Some(self.start)
    }
 
-   fn reset(&mut self) -> Option<()> {
-      debug_assert!(!self.stack.is_empty());
-      self.peek = self.stack.last().unwrap();
+   fn adjust(&mut self, pos: usize) -> Option<usize> {
+      debug_assert!(self.start != pos);
+      self.start = pos;
+      self.peek = pos;
+      Some(pos)
+   }
+
+   fn reset(&mut self) -> Option<usize> {
+      self.peek = self.start;
       None
    }
 
-   fn debug(&self, moment: &str) {
-      if let Some(first) = self.peek.first() {
-         println!("{} / {:?}", moment, first);
-      } else {
-         println!("{} / none", moment);
-      }
-   }
-
-   fn advance(&mut self, delta: usize) -> Option<()> {
-      self.peek = &self.peek[delta..];
-      Some(())
-   }
-
    fn remaining(&self) -> usize {
-      debug_assert!(!self.stack.is_empty());
-      self.stack.last().unwrap().len()
+      self.input.len() - self.start
    }
 
-   fn next_fn(&mut self, f: fn(item: &T) -> bool) -> Option<()> {
-      if let Some(first) = self.peek.first() {
-         if f(first) {
-            return self.advance(1);
+   #[allow(dead_code)]
+   fn next_fn(&mut self, f: fn(item: &Syn) -> bool) -> Option<usize> {
+      if let Some(current) = self.current() {
+         if f(current) {
+            return self.step();
          }
       }
       self.reset()
    }
 
-   fn next(&mut self, item: &T) -> Option<()> {
-      if let Some(first) = self.peek.first() {
-         if *first == *item {
-            return self.advance(1);
+   fn next(&mut self, item: &Syn) -> Option<usize> {
+      if let Some(current) = self.current() {
+         if *current == *item {
+            return self.step();
          }
       }
       self.reset()
    }
 
-   fn optional_from_slice(&mut self, items: &[&T]) -> bool {
-      if let Some(first) = self.peek.first() {
+   fn optional_from_slice(&mut self, items: &[&Syn]) -> Option<usize> {
+      if let Some(current) = self.current() {
          for item in items {
-            if *first == **item {
-               self.advance(1);
-               return true;
+            if *current == **item {
+               return self.step();
             }
          }
       }
-      false
+      None
    }
 
-   fn optional(&mut self, item: &T) -> bool {
-      if let Some(first) = self.peek.first() {
-         if *first == *item {
-            self.advance(1);
-            return true;
+   fn optional(&mut self, item: &Syn) -> Option<usize> {
+      if let Some(current) = self.current() {
+         if *current == *item {
+            return self.step();
          }
       }
-      false
+      None
+   }
+
+   fn descend(&mut self, f: fn(&mut Peeker) -> Option<usize>) -> Option<usize> {
+      let mut peeker = self.clone();
+      self.peek = f(&mut peeker)?;
+      Some(self.peek)
    }
 }
