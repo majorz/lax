3510c5cc3d0326fb554241d7d3745c557bad646e
majorz <zahari@resin.io>
2018-02-23 23:29:08 +0200

    Simplifications

--- examples/grammar.rs
+++ examples/grammar.rs
@@ -9,8 +9,13 @@ fn main() {
 
    number(&mut builder);
 
-   builder
-      .nodes
+   number_with_integer(&mut builder);
+
+   number_fractional_only(&mut builder);
+
+   let nodes = builder.destructure();
+
+   nodes
       .iter()
       .enumerate()
       .for_each(|(i, node)| println!("[{:03}] {:?}", i, node));
@@ -21,26 +26,40 @@ fn number(builder: &mut Builder) {
    builder
       .element(Element::Number)
          .choice()
-            .sequence()
-               .tok(Tok::Digits)
-               .zero_or_one()
-                  .tok(Tok::FullStop)
-                  .zero_or_one()
-                     .tok(Tok::Digits)
-                  .end()
-               .end()
-            .end()
-            .sequence()
-               .tok(Tok::FullStop)
+            .reference(Element::NumberWithInteger)
+            .reference(Element::NumberFractionalOnly)
+         .end()
+      .end();
+}
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn number_with_integer(builder: &mut Builder) {
+   builder
+      .element(Element::NumberWithInteger)
+         .tok(Tok::Digits)
+         .zero_or_one()
+            .tok(Tok::FullStop)
+            .zero_or_one()
                .tok(Tok::Digits)
             .end()
          .end()
       .end();
 }
 
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn number_fractional_only(builder: &mut Builder) {
+   builder
+      .element(Element::NumberFractionalOnly)
+         .tok(Tok::FullStop)
+         .tok(Tok::Digits)
+      .end();
+}
+
 #[derive(Debug)]
 enum Element {
    Number,
+   NumberWithInteger,
+   NumberFractionalOnly,
    Space,
    Identifier,
    Single,
@@ -53,7 +72,7 @@ enum Element {
 #[derive(Debug)]
 enum Node {
    Element(Element),
-   ElementRef(Element),
+   Reference(Element),
    Tok(Tok),
    Sequence,
    Choice,
@@ -74,14 +93,22 @@ impl Builder {
       }
    }
 
+   fn destructure(self) -> Vec<Node> {
+      let Self {
+         nodes, ..
+      } = self;
+
+      nodes
+   }
+
    fn element(&mut self, element: Element) -> &mut Self {
       self.start();
       self.nodes.push(Node::Element(element));
       self
    }
 
-   fn element_ref(&mut self, element: Element) -> &mut Self {
-      self.nodes.push(Node::ElementRef(element));
+   fn reference(&mut self, element: Element) -> &mut Self {
+      self.nodes.push(Node::Reference(element));
       self
    }
 
@@ -126,13 +153,18 @@ impl Builder {
 
 
 number = |
-   ~
-      one(^digits)
-      zero_or_one(
-         ^full_stop
-         zero_or_one(^digits)
-      )
-   one(^full_stop) one(^digits)
+   number_with_integer
+   number_fractional_only
+
+number_with_integer = (
+   one(^digits)
+   zero_or_one(
+      ^full_stop
+      zero_or_one(^digits)
+   )
+)
+
+number_fractional_only = one(^full_stop) one(^digits)
 
 space = zero_or_one(^space)
 
